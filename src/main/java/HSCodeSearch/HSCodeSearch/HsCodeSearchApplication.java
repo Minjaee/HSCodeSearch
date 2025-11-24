968877bf32cf90e94b04dfefe6d5e067da8cd93e
Minjaee <chelsea@jbnu.ac.kr>
2025-11-24 17:45:06 +0900

    검색창UI

---
+++ src/main/java/HSCodeSearch/HSCodeSearch/HsCodeSearchApplication.java
@@ -0,0 +1,13 @@
+package HSCodeSearch.HSCodeSearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsCodeSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HsCodeSearchApplication.class, args);
+	}
+
+}
